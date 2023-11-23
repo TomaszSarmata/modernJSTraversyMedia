66a159392df476b9607ac0f1dc3f527c93807453
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-23 21:32:23 +0000

    a new module added

---
+++ 01-variables-dataTypes/03-variables/script.js
@@ -0,0 +1,39 @@
+// console.log(typeof 100);
+
+/*
+multi-line commenting
+
+console.error("Alert");
+
+console.warn("warning");
+
+console.table({ name: "brad", email: "brad@gmail.com" });
+
+shit up/down/left/right - highlights the code
+cmd up/left/down/right - moves cursor by the whole line / bloke of code
+option left/right - moves cursor by a word
+option up/down - will move the line of code up or down
+shift option left/right - will highlight the code horizontally
+shift cmd left/right - will highlight the code from where you are over
+shift option down - will duplicate the line of code
+shift option down - will duplicate the line of code
+cmd z - will undo
+cmd d - will move cursor in multiple instance of the highlighted word (so first highlight the word and then cmd d)
+cmd shift l - will highlight all instances of the highlighted word (instead of moving one by one with the cmd d)
+option click will select multiple cursors
+cmd shift o - will search for the file
+cmd option f - will search for the word / phrase in the main text editor / main file
+cmd shift f - will search the word in the entire folder/project (or click magnifying glass)
+cmd b - will toggle the side bar
+ctrl ~ - will toggle the terminal
+
+*/
+
+console.group("simple");
+console.log(typeof 100);
+console.error("Alert");
+console.warn("warning");
+// console.groupEnd;
+
+const styles = "padding: 10px; background-color: black; color: green";
+console.log("%cHello World", styles);
