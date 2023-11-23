0cf7c39524afd4d0d340096bab2ee31188c088b9
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-23 22:37:31 +0000

    explenation on how data types are store either in the stack or in the heap and how it affects reassignment process

--- 01-variables-dataTypes/04-dataTypes/script.js
+++ 01-variables-dataTypes/04-dataTypes/script.js
@@ -35,3 +35,22 @@ function sayHello() {
 }
 const output1 = sayHello;
 console.log(output1, typeof output1);
+
+//these values (primitive) are stored on the stack
+
+const imie = "tom";
+const wiek = 38;
+
+//reference values are stored on the heap
+
+const osoba = {
+  name: "tom",
+  age: 40,
+};
+
+let noweImie = imie;
+noweImie = "John";
+
+let nowaOsoba = osoba;
+nowaOsoba.name = "Bradley";
+console.log(osoba, nowaOsoba); //both osoba and nowaOsoba will change the name to Bradley as that info is kept in the heap
